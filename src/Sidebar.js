a5e1a8537242978d412d44a7aff00c34dd445bc8
chamika-damith <chamikadamith9@gmail.com>
2024-09-12 00:14:33 +0530

    update sidebar section

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,10 +5,31 @@ import { Avatar } from '@mui/material';
 function Sidebar() {
   return (
     <div className='sidebar'>
-        <h1>I am a sidebar</h1>
         <div className='sidebar__top'>
-            <img src='' alt=''/>
-            <Avatar />
+            <img src='https://img.freepik.com/free-photo/vivid-blurred-colorful-background_58702-2545.jpg?w=1060&t=st=1726075012~exp=1726075612~hmac=cebd5bc6f6e10d49b2dea451e6a0368f0b2ff2476aacd2b8a4d3daa7c38d5c62' alt=''/>
+            <Avatar className='sidebar__avatar'/>
+            <h2>Chamika damith</h2>
+            <h4>Chamikadamith9gmail.com</h4>
+
+            <div className='sidebar__stats'>
+                <div className='sidebar__stat'>
+                    <p>who viewed you</p>
+                    <div className='sidebar__statNumber'>
+                        2,534
+                    </div>
+                </div>
+
+                <div className='sidebar__stat'>
+                <p>views on post</p>
+                    <div className='sidebar__statNumber'>
+                        2,442
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className='sidebar__buttom'>
+            <p>Recent</p>
         </div>
     </div>
   )
