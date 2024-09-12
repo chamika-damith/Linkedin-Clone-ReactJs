65c7f4c9a0d2957fb2684b712888a5d795b83172
chamika-damith <chamikadamith9@gmail.com>
2024-09-12 15:13:47 +0530

    create feed section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       {/*App Body*/}
       <div className='app__body'>
         <Sidebar />
+        <Feed />
       </div>
     </div>
   );
