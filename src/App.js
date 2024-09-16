dad46dec890db697261fc22c52c63804a1e1ceb7
chamika-damith <chamikadamith9@gmail.com>
2024-09-16 11:26:01 +0530

    create Feed & Post section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Post from './Post';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <div className='app__body'>
         <Sidebar />
         <Feed />
+        <Post />
       </div>
     </div>
   );
